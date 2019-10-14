b906ca968964fb5a54cb40faaed509f7ed5f1c29
Sanjeev <sanjeev@iisl.co.in>
2019-10-14 15:58:01 +0530

    updated eval and test

--- notebook/pytorch/test.py
+++ notebook/pytorch/test.py
@@ -7,14 +7,14 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-from data import VOC_ROOT, VOC_CLASSES as labelmap
+from data import VOC_ROOT, LEAF_CLASSES as labelmap
 from PIL import Image
-from data import VOCAnnotationTransform, VOCDetection, BaseTransform, VOC_CLASSES
+from data import VOCAnnotationTransform, VOCDetection, BaseTransform, LEAF_CLASSES
 import torch.utils.data as data
 from ssd import build_ssd
 
 parser = argparse.ArgumentParser(description='Single Shot MultiBox Detection')
-parser.add_argument('--trained_model', default='weights/ssd_300_VOC0712.pth',
+parser.add_argument('--trained_model', default='weights/ssd300_LEAF_90.pth',
                     type=str, help='Trained state_dict file path to open')
 parser.add_argument('--save_folder', default='eval/', type=str,
                     help='Dir to save results')
@@ -78,7 +78,7 @@ def test_net(save_folder, net, cuda, testset, transform, thresh):
 
 def test_voc():
     # load net
-    num_classes = len(VOC_CLASSES) + 1 # +1 background
+    num_classes = len(LEAF_CLASSES) + 1 # +1 background
     net = build_ssd('test', 300, num_classes) # initialize SSD
     net.load_state_dict(torch.load(args.trained_model))
     net.eval()
